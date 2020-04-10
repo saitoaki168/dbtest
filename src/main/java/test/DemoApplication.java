4b2bb8ed6e3d6c8cfd7f8f10220f299f5a6cd6d0
saitoaki168 <59859271+saitoaki168@users.noreply.github.com>
2020-04-10 20:07:10 +0900

    Update DemoApplication.java

--- src/main/java/test/DemoApplication.java
+++ src/main/java/test/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-
+	
 	public static void main(final String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
